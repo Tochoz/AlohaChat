c24db258a4a3a1fadc682fd64256dcedefe1f196
Сергей Пугач <sergey.pugach02@gmail.com>
2021-09-11 19:41:15 +0300

    Imrove chat appearance

--- main/messanger.py
+++ main/messanger.py
@@ -6,10 +6,10 @@ def send_message(request):
     name = request.session['name']
     content = request.POST['content']
     payload = {'name': name, 'content': content}
-    print(f"got message: {payload}")
+    #print(f"got message: {payload}")
     message = Message.objects.create(name=name, content=content)
     message.save()
-    print(f"message: {payload} saved")
+    #print(f"message: {payload} saved")
     return redirect(reverse('home'))
 
 def homepage(request):
@@ -17,8 +17,8 @@ def homepage(request):
         #name = request.GET['name']
         messages = []
         for message in Message.objects.all():
-            messages.append(f"{message.name}:{message.timestamp}<br>{message.content}")
-        print(f"messages: {messages}")
+            messages.append({'info': f"{message.name} {str(message.timestamp)[:-16]}:", 'content': message.content})
+        #print(f"messages: {messages}")
         name = request.session["name"]
         return render(request, 'main/home.html', {'name': name, "messages": messages})
     return redirect('/')
